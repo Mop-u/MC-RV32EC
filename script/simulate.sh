d7195d0b4116fb6e81fa2b619ca70b017e829fd9
Moppu <moppu@pm.me>
2021-01-25 14:47:56 +0000

    more tweaks to the build scripts to play nicely with windows/sublime text

--- script/simulate.sh
+++ script/simulate.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 if [ -z "$1" ]
 then
-    echo "Provide the name of the desired toplevel testbench in the rtl directory!"
+    echo "simulate.sh needs the name of a toplevel testbench from the rtl directory!"
 else
     toplevel=${1%.*}
     toplevel=${toplevel##*/}
     toplevel=${toplevel##*\\} # just in case a windows path was passed...
-    tmp="$PWD/../tmp"
+    getmypath () {
+        echo ${0%/*}/
+    }
+    mypath=$(getmypath)
+    tmp="$mypath/../tmp"
     convertfile () {
         modname=${1%.*}
         modname=${modname##*/}
-        sv2v $1 > $tmp/$modname.v
+        sv2v $1 > $tmp/$modname.sv
     }
     find ../rtl/ -iname "*.*v" | while read file; do convertfile "$file"; done
-    iverilog -o $tmp/out.tmp -g2012 -grelative-include -y $tmp $tmp/$toplevel.v
+    iverilog -o $tmp/out.tmp -g2012 -grelative-include -Y .sv -y $tmp $tmp/$toplevel.sv
     vvp $tmp/out.tmp
 fi
--- src/compile.sh
+++ src/compile.sh
@@ -3,7 +3,11 @@ if [ -z "$1" ]
 then
     echo "compile.sh needs an assembly source!"
 else
+    getmypath () {
+        echo ${0%/*}/
+    }
+    mypath=$(getmypath)
     riscv64-unknown-elf-as -march=rv32ic $1 -o rom.o
-    riscv64-unknown-elf-objcopy -O binary -j .text rom.o ../rom/rom.bin
+    riscv64-unknown-elf-objcopy -O binary -j .text rom.o $mypath/../rom/rom.bin
     rm rom.o
 fi
